a2818f05d0a5f727cb4dcbf55e33bf2ecc0673fe
SingleMoonlight <winston.wyx@foxmail.com>
2024-03-01 23:40:40 +0800

    Modify domain name

--- src/stores/setting.js
+++ src/stores/setting.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useSettingStore = defineStore('setting', {
     state: () => ({
-        backgroundUrl: 'https://cdn.smilingly.cn/website/qhGuide/guide_default_bg.jpg',
+        backgroundUrl: 'https://cdn.ifback.com/website/qhGuide/guide_default_bg.jpg',
         showTime: true,
         showSecond: false,
         blinkSemicolon: true,
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,13 +1,13 @@
 export const version = '3.0.0'
-export const defaultBackground = 'https://cdn.smilingly.cn/website/qhGuide/guide_default_bg.jpg'
+export const defaultBackground = 'https://cdn.ifback.com/website/qhGuide/guide_default_bg.jpg'
 
 export const copyrightInfo = {
-    ownerInfo: 'smilingly.cn',
-    ownerUrl: 'https://www.smilingly.cn/',
+    ownerInfo: 'ifback.com',
+    ownerUrl: 'https://ifback.com/',
     beianProvince: '豫',
     gonanBeianNum: '41152402000212号',
-    gonanBeianUrl: 'http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=41152402000212',
-    icpBeianNum: '2021014629号-1',
+    gonanBeianUrl: 'http://www.beian.gov.cn/',
+    icpBeianNum: '2021014629号-2',
     icpBeianUrl: 'https://beian.miit.gov.cn/',
 }
 
